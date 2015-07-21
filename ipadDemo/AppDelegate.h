2373fa265a8776d0a0a26bf290f2b81b45e2af90
Bigjinbin <2213620570@qq.com>
2015-07-21 09:52:24 +0800

    Initial Commit

---
+++ ipadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ipadDemo
+//
+//  Created by jb on 15/7/21.
+//  Copyright (c) 2015年 jb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
